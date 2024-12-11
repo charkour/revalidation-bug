e5a8e7e4b355e2b0e2c98b97a574de27486b3bd0
Charles Kornoelje <33156025+charkour@users.noreply.github.com>
2024-12-11 15:35:17 -0500

    update log

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,7 +2,7 @@ import { REVALIDATE_EVERY_MINUTE } from "const";
 
 export const getStaticProps = async (context) => {
     const timestamp = new Date().toISOString();
-    console.log('rendered at', timestamp);
+    console.log('rendered at', timestamp, 'on preview deployment');
     return { props: { timestamp }, revalidate: REVALIDATE_EVERY_MINUTE };
 };
 
